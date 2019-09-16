7ab7242440fd655437caf5d6c729d5dd6ce8e99b
davidorg15 <david.deoliveira-rodrigues-gomes@etu.hesge.ch>
2019-09-16 23:29:39 +0200

    16.09.19, Résolution ex 9.1

--- 9.1- Exercice - Traitement de string/string1.py
+++ 9.1- Exercice - Traitement de string/string1.py
@@ -1,2 +1,7 @@
 # Consigne : Récupérer le mot "chaine" du string s et l'afficher
 s = 'un exemple de chaine'
+#sousChaine = s[14:20]
+#print (sousChaine)
+i = s.find('chaine')
+sousChaine = s[i:]
+print (sousChaine)
