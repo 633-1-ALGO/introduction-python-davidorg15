c6bd09a0f0cf2b6c215e586df98eff57c97c28cf
davidorg15 <david.deoliveira-rodrigues-gomes@etu.hesge.ch>
2019-09-16 23:45:24 +0200

    16.09.19, Résolution ex 9.2

--- 9.2  Exercice - Traitement de string/string2.py
+++ 9.2  Exercice - Traitement de string/string2.py
@@ -1,3 +1,17 @@
 # Consigne : Rechercher le nombre d'occurences du mot "exemple" et l'afficher. Remplacer le mot "est" par "représente".
 # Bonus : Inverser le sens de lecture.
+
+#Compter nombre d'occurences du mot "exemple" et l'afficher.
 texte = "Ceci est un exemple exemplaire d'exemple exempté d'exemple."
+nbExemple = texte.count("exemple")
+print("Le nombre est ",nbExemple)
+
+#Remplacer le mot "est" par "représente".
+texteRemplace = texte.replace('est','représente')
+print(texteRemplace)
+
+# Inverser le sens de lecture
+texteInverse = ""
+for lettre in reversed(texte):
+    texteInverse +=lettre
+print(texteInverse)
